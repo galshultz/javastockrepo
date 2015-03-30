d753931491f08f3a009cc80fc2545e1cda4e7aed
galshultz <galshultz88@gmail.com>
2015-03-30 19:02:32 +0300

    ex2 - math

--- src/com/shultz/StockPortfolioJavaCourseServlet.java
+++ src/com/shultz/StockPortfolioJavaCourseServlet.java
@@ -5,7 +5,13 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class StockPortfolioJavaCourseServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1 =4, num2 = 3, num3 = 7;
+		int result = 0 ;
+				
+		result = (num1+num2)*num3;
+		String resultString = new String("<h1>Result of ("+num1+"+"+num2+")*"+num3+" = "+result+"</h1>");
+		
+		resp.getWriter().println(resultString);
 	}
 }
