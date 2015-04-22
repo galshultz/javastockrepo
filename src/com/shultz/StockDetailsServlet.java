e440becfbc06992131be38939dd4da9f37db9b27
galshultz <galshultz88@gmail.com>
2015-04-22 16:24:21 +0300

    updated appengine version to 5

--- src/com/shultz/StockDetailsServlet.java
+++ src/com/shultz/StockDetailsServlet.java
@@ -7,8 +7,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.util.*;
 
+@SuppressWarnings("serial")
 public class StockDetailsServlet extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
